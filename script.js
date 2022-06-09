31cfaa4af4ec941e8510ec3b2c9fd18c5cb407b6
AnBunchuk <av.bunchuk@gmail.com>
2022-06-10 00:16:06 +0300

    работает калькулятор валют

--- script.js
+++ script.js
@@ -11,66 +11,105 @@ class Calc {
         this.change = this.wrapper.querySelector('.change');
         this.currency_change1 = this.wrapper.querySelector('.currency_change1');
         this.currency_change2 = this.wrapper.querySelector('.currency_change2');
-        this.cashValue = '1';
-        this.currency1 = '1';
-        this.currency2 = '40';
+        this.cashValue = '1'; // нал-безнал
+        this.currency1 = '1';// верхняя валюта currency_change1
+        this.currency2 = '40';// нижняя валюта currency_change2
+        this.dataServer = [];
     }
-
+    // текущее число
     currentDate() {
         let todey = new Date();
         let myMonth = todey.getMonth();
         // console.log(todey.getMonth());
-        if (myMonth < 10) myMonth = '0' + myMonth;
+        if (myMonth < 10) myMonth = '0' + ++myMonth;
         let myDate = todey.getDate() + '.' + myMonth + '.' + todey.getFullYear();
         this.date.innerText = myDate;
     }
+    // поле нал-безнал
     cashNonCash() {
         this.nalBeznal.oninput = (e) => {
             let cash = e.target.selectedIndex;
             if (cash === 0) this.cashValue = '1';
             if (cash === 1) this.cashValue = '1.3';
-            console.log(this.cashValue)
+            // console.log(this.cashValue)
         }
     }
+    // поле меняю
     currencyChange1() {
         this.currency_change1.oninput = (e) => {
             let currency = e.target.selectedIndex;
-            if (currency === 0) this.currency1 = '1';
-            if (currency === 1) this.currency1 = '1.3';
-            if (currency === 2) this.currency1 = '40';
-            console.dir(this.currency1)
+            if (currency === 0) this.currency1 = this.dataServer[0].buy //usd '29.25490'
+            if (currency === 1) this.currency1 = this.dataServer[1].buy; //eur '31.19010'
+            if (currency === 2) this.currency1 = 1;
+            // console.dir(this.currency1)
         }
     }
+    // поле получаю
     currencyChange2() {
         this.currency_change2.oninput = (e) => {
             let currency = e.target.selectedIndex;
-            if (currency === 0) this.currency2 = '1';
-            if (currency === 1) this.currency2 = '1.3';
-            if (currency === 2) this.currency2 = '40';
-            console.dir(this.currency2)
+            // if (currency === 0) this.currency2 = '29.25490'; 
+            if (currency === 0) this.currency2 = 1 / this.dataServer[0].sale; //usd '32.78689'
+            // if (currency === 1) this.currency2 = '31.19010'; 
+            if (currency === 1) this.currency2 = 1 / this.dataServer[1].sale; //eur '35.08772'
+            if (currency === 2) this.currency2 = 1;
+            // console.dir(this.currency2)
         }
     }
 
-
     dataInput() {
         this.wrapper.addEventListener('click', (e) => {
-            console.dir(e.target)
-
+            // console.dir(e.target)
+            // нажатие кнопки "=" 
             if (e.target.value === "=")
                 this.calculation()
         })
     }
     calculation() {
-      let rez =  this.cashValue* (this.currency1*this.money1.value*this.currency2)
-      console.log(rez)
-      this.money2.value=rez;
+            let rez = this.cashValue * (this.currency1 * this.money1.value * this.currency2)
+            this.money2.value = rez;
     }
+
+    dataPrivat() {
+        // let dataServer = [];
+
+        // создаем обьект для получаем данных с сервера
+        const request = new XMLHttpRequest();
+
+        // запрос на сервер откуда будем получать и в каком виде запрос
+        request.open("GET", "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11")
+        // настройка заголока нашего соединения, т.е. в каком формате мы хотим получить данные
+        // request.setRequestHeader('Content-type', 'application/json')// 'этот стандартный но не заработал!!!
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        // производим само соединение
+        request.send()
+
+        // создаем слушатель когда данные будут получены
+        request.addEventListener('readystatechange', (event) => {
+            // ждем статуса "получен"
+            if (request.readyState === 4 && request.status === 200) {
+                // получаем текст сообщения в виде строки
+                this.dataServer = request.responseText;
+                // преобразуем к формату JSON
+                this.dataServer = JSON.parse(this.dataServer)
+                console.log(this.dataServer);
+// подставляем данные
+this.currency1=this.dataServer[0].buy
+// this.currency_change2[0].selected=false
+this.currency_change2[2].selected=true
+this.currency2 = 1
+
+            }
+        })
+    }
+
 
 
 
 
     init() {
         console.dir(this);
+        this.dataPrivat()
         this.currentDate();
         this.cashNonCash();
         this.currencyChange1();
